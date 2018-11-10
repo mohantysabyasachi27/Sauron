65f2541e36ac17d1104449703e9c2ba859820334
utkagarwal <utkag789@gmail.com>
2018-11-10 11:29:16 -0700

    40%

---
+++ Client/traffic/src/app/app-component-service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor(private http: HttpClient) { }
+  readonly endpoint = 'http://18.237.252.206:8080/';
+  readonly httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+  getCategories():void{}
+
+  postReview():void{}
+
+  getPending():void{}
+
+  getHistory():void{}
+
+  getLatLng():void{}
+
+  postSignUp():void{}
+
+  postLogin():void{}
+}
